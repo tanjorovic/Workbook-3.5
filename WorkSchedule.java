1b0ad4b72d240eac48e2627d3e02c75b1101dbe9
Amjed Tdj <amjedtadjine2@gmail.com>
2023-10-12 23:16:59 +0200

    first commit

---
+++ WorkSchedule.java
@@ -0,0 +1,21 @@
+public class WorkSchedule {
+    public static void main(String[] args) {
+        
+        //See Learn the Part for the complete instructions (link in resources folder of Udemy video).  
+
+        int day = 3;   //3rd day of the week...
+        boolean holiday = true;
+         
+        // IF - ELSE IF - ELSE HERE!
+        if (holiday){
+            System.out.println("Woohoo, no work!");
+        }else if (day>=6){
+            System.out.println("Its the weekend, no work!");
+        }else {
+            System.out.println("Wake up at 7:00 :(");
+        }
+        
+        
+        }
+
+    }
